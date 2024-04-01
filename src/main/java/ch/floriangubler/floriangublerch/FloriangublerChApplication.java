0c2dab447ffdd97857d5a682a138f4e5901517d5
Florian Gubler <70200329+FlorianGubler@users.noreply.github.com>
2024-04-01 15:47:27 +0200

    init

---
+++ src/main/java/ch/floriangubler/floriangublerch/FloriangublerChApplication.java
@@ -0,0 +1,13 @@
+package ch.floriangubler.floriangublerch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FloriangublerChApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FloriangublerChApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ch/floriangubler/floriangublerch/FloriangublerChApplicationTests.java
@@ -0,0 +1,13 @@
+package ch.floriangubler.floriangublerch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FloriangublerChApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
