161674882e1f27d0f58ebdc57be0b85c5434ea3e
Florian Gubler <70200329+FlorianGubler@users.noreply.github.com>
2024-04-26 17:38:35 +0200

    fix actuator startup

--- src/main/java/ch/floriangubler/floriangublerch/FloriangublerChApplication.java
+++ src/main/java/ch/floriangubler/floriangublerch/FloriangublerChApplication.java
@@ -2,12 +2,15 @@ package ch.floriangubler.floriangublerch;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.metrics.buffering.BufferingApplicationStartup;
 
 @SpringBootApplication
 public class FloriangublerChApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(FloriangublerChApplication.class, args);
+		SpringApplication app = new SpringApplication(FloriangublerChApplication.class);
+		app.setApplicationStartup(new BufferingApplicationStartup(2048));
+		app.run(args);
 	}
 
 }
